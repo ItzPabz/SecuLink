34ea61062028fa26516744eb79289c4afddfdfbc
ItzPabz <pablojdavid02@gmail.com>
2023-10-17 18:45:53 -0400

    Made Basic Starting Screen
    - Removed Themes
    - Added "Login" Page
    - Added Data storage via JSON

--- main.py
+++ main.py
@@ -2,78 +2,82 @@
 import flet as ft
 from flet import *
 from cryptography.fernet import Fernet
+import json, os
 
-def main(page: Page):
-    ## Window Settings ##
+def main(page: ft.Page) -> None:
     page.title = "VaultTalk"
-    page.window_title_bar_hidden = False
-    page.window_title_bar_buttons_hidden = False
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.window_maximizable = False
-    page.window_resizable = True
+    page.window_resizable = False
     page.window_width = 450
     page.window_height = 600
     page.theme = ft.Theme(color_scheme_seed='blue')
 
-    ## Functions ##
-    def themechange(e):
-        page.theme = ft.Theme(color_scheme_seed=dd_Theme.value)
+    # Setup Login Fields
+    tx_Header: Text = Text(value='VaultTalk', size=34, width=300, text_align=ft.TextAlign.CENTER)
+    tx_tagline: Text = Text(value='Sleek • Fast • Encrpyted', size=16, width=300, text_align=ft.TextAlign.CENTER)
+    tf_nickname: TextField = TextField(label='Nickname', text_align=ft.TextAlign.LEFT, width=300)
+    tf_key: TextField = TextField(label='Private Key', text_align=ft.TextAlign.LEFT, width=300, password=True)
+    btn_CreateAcct: ElevatedButton = ElevatedButton(text='Create Account', width=300, disabled=True)
+
+    def validateFields(e: ControlEvent) -> None:
+        if all([tf_nickname.value, tf_key.value]):
+            btn_CreateAcct.disabled = False
+        else:
+            btn_CreateAcct.disabled = True
+        
         page.update()
 
-    def close_app(e):
-        page.window_destroy()
 
 
+    def createAccount(e: ControlEvent) -> None:
+        json_data = {"nickname": tf_nickname.value, "privateKey": tf_key.value}
+        with open('user-info.json', 'w', encoding='utf-8') as f:
+            json.dump(json_data, f, ensure_ascii=False, indent=4)
 
-    ## Items ##
-    btn_StartSession = ft.ElevatedButton(
-        content=ft.Container(
-                content=ft.Column(
-                    [
-                        ft.Text(value="START SESSION", size=20),    
-                        ft.Text(value="start a new encrypted chating session", size=10),
-                    ],
-                    alignment=ft.MainAxisAlignment.SPACE_AROUND,
-                    spacing=5,
-                ),
-                padding=ft.padding.all(10),
+        page.clean()
+        page.add(
+            Row(
+                controls=[Text(value=f"Welcome {tf_nickname.value}", size=20)],
+                alignment=ft.MainAxisAlignment.CENTER
+            )
         )
-    )
-    
-    btn_JoinSession = ft.ElevatedButton("Join Session")
-
-    dd_Theme = ft.Dropdown(
-        on_change=themechange,
-        hint_text="Theme",
-        width=125,
-        options=[
-            ft.dropdown.Option("blue"),
-            ft.dropdown.Option("green"),
-            ft.dropdown.Option("red"),
-            ft.dropdown.Option("purple"),
-            ft.dropdown.Option("orange"),
-            ft.dropdown.Option("yellow"),
-            ft.dropdown.Option("pink"),
-        ]
-    )
-
-    btn_Close = ft.ElevatedButton("CLOSE",
-        icon="close",
-        icon_color="red",
-        color="red",
-        bgcolor="white",
-        on_click=close_app,
-    )
-    
-    page.add(btn_StartSession) 
-    page.add(btn_JoinSession)
-    page.add(dd_Theme)
-    page.add(btn_Close)
-    page.update()
-    pass
-
-if __name__ == "__main__":
-        ft.app(target=main)
 
 
+
+    # Link Functions to UI
+    tf_nickname.on_change = validateFields
+    tf_key.on_change = validateFields
+    btn_CreateAcct.on_click = createAccount
+
+    #Renger Page
+    if os.path.isfile('user-info.json'):
+        page.add(
+            Row(
+                controls=[Text(value=f"Welcome {tf_nickname.value}", size=20)],
+                alignment=ft.MainAxisAlignment.CENTER
+            )
+        )
+    else:
+        page.add(
+            Row(
+                controls=[
+                    Column(
+                        [
+                        tx_Header,
+                        tx_tagline,
+                        tf_nickname, 
+                        tf_key,
+                        btn_CreateAcct
+                        ]
+                    )
+                ],
+                alignment=ft.MainAxisAlignment.CENTER
+            )
+        )
+
+
+if __name__ == '__main__':
+    ft.app(main)
 
 
