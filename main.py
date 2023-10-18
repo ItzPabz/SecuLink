772cf882a53c8535924cbfbb31e10995fd59d25d
ItzPabz <64668341+ItzPabz@users.noreply.github.com>
2023-10-18 18:12:58 -0400

    Added Network Functionality
    Basic Server Client COmmunication,
    
    Only works on LAN

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #VaultTalk
 import flet as ft
 from flet import *
-from cryptography.fernet import Fernet
-import json, os
+import socket
+        
 
 def main(page: ft.Page) -> None:
     page.title = "VaultTalk"
@@ -14,12 +14,25 @@ def main(page: ft.Page) -> None:
     page.theme = ft.Theme(color_scheme_seed='blue')
 
     # Setup Login Fields
+    # INITIAL PAGE
     tx_Header: Text = Text(value='VaultTalk', size=34, width=300, text_align=ft.TextAlign.CENTER)
     tx_tagline: Text = Text(value='Sleek • Fast • Encrpyted', size=16, width=300, text_align=ft.TextAlign.CENTER)
     tf_nickname: TextField = TextField(label='Nickname', text_align=ft.TextAlign.LEFT, width=300)
     tf_key: TextField = TextField(label='Private Key', text_align=ft.TextAlign.LEFT, width=300, password=True)
     btn_CreateAcct: ElevatedButton = ElevatedButton(text='Create Account', width=300, disabled=True)
 
+    # CONNECTION PAGE
+    tx_Connect: Text = Text(value='Connect to a Host', size=24, width=300, text_align=ft.TextAlign.CENTER)
+    tf_ipfield: TextField = TextField(label='Host IP Address', text_align=ft.TextAlign.CENTER, width=300)
+    btn_Connect: ElevatedButton = ElevatedButton(text='Connect', width=300, disabled=False)
+    tx_Host: Text = Text(value='Become a Host', size=24, width=300, text_align=ft.TextAlign.CENTER)
+    btn_Host: ElevatedButton = ElevatedButton(text='Host', width=300)
+
+
+
+
+
+    # Functions
     def validateFields(e: ControlEvent) -> None:
         if all([tf_nickname.value, tf_key.value]):
             btn_CreateAcct.disabled = False
@@ -28,20 +41,47 @@ def main(page: ft.Page) -> None:
         
         page.update()
 
-
-
     def createAccount(e: ControlEvent) -> None:
-        json_data = {"nickname": tf_nickname.value, "privateKey": tf_key.value}
-        with open('user-info.json', 'w', encoding='utf-8') as f:
-            json.dump(json_data, f, ensure_ascii=False, indent=4)
-
         page.clean()
         page.add(
-            Row(
-                controls=[Text(value=f"Welcome {tf_nickname.value}", size=20)],
-                alignment=ft.MainAxisAlignment.CENTER
-            )
+        Row(
+            controls=[
+                Column(
+                    [
+                    tx_Header,
+                    tx_Connect,
+                    tf_ipfield,
+                    btn_Connect,
+                    tx_Host,
+                    btn_Host,
+                    ]
+                )
+            ],
+            alignment=ft.MainAxisAlignment.CENTER
         )
+    )
+        
+    def start_server(e: ControlEvent) -> None:
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host = socket.gethostname()
+        port = 8765
+        server_socket.bind((host, port))
+        server_socket.listen(1)
+        print("Waiting for connection...")
+        client_socket, addr = server_socket.accept()
+        print("Connection from " + str(addr))
+        data = client_socket.recv(1024)
+        print("Received: " + str(data))
+        client_socket.close()
+
+    def connect(e: ControlEvent) -> None:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host = tf_ipfield.value
+        port = 8765
+        client_socket.connect((host, port))
+        message = "Hello Server!"
+        client_socket.send(message.encode())
+        client_socket.close()
 
 
 
@@ -50,31 +90,26 @@ def main(page: ft.Page) -> None:
     tf_key.on_change = validateFields
     btn_CreateAcct.on_click = createAccount
 
+    btn_Host.on_click = start_server
+    btn_Connect.on_click = connect
+
     #Renger Page
-    if os.path.isfile('user-info.json'):
-        page.add(
-            Row(
-                controls=[Text(value=f"Welcome {tf_nickname.value}", size=20)],
-                alignment=ft.MainAxisAlignment.CENTER
-            )
+    page.add(
+        Row(
+            controls=[
+                Column(
+                    [
+                    tx_Header,
+                    tx_tagline,
+                    tf_nickname, 
+                    tf_key,
+                    btn_CreateAcct
+                    ]
+                )
+            ],
+            alignment=ft.MainAxisAlignment.CENTER
         )
-    else:
-        page.add(
-            Row(
-                controls=[
-                    Column(
-                        [
-                        tx_Header,
-                        tx_tagline,
-                        tf_nickname, 
-                        tf_key,
-                        btn_CreateAcct
-                        ]
-                    )
-                ],
-                alignment=ft.MainAxisAlignment.CENTER
-            )
-        )
+    )
 
 
 if __name__ == '__main__':
